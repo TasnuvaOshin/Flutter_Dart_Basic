3804b00bcada09a275f59a84fe2f141514fe24ef
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-10-08 18:47:50 +0600

    text design

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,21 @@ backgroundColor: Colors.yellow,
 
 
 body: Center(
-child: Text("Hello Oshin"),
+child: Text(
+  "Todays day two of Flutter and Dart , lets learn with fun..",
+     style: TextStyle(
+        color: Colors.green,
+        fontSize: 40.5,
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.bold,
+        wordSpacing: 5.6,
+        fontFamily: "Times New Roman",
+     ),    
+
+    
+
+
+),
   
 ),
      ),
