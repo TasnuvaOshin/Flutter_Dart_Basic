e7cf8b1b450fa0e3c5aace6ca37a1fdb374dd0c1
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-10-16 15:39:21 +0600

    Stateful Button CLick to Change the background Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,123 @@
-            import 'package:flutter/material.dart';
-            void main(){
+              import 'package:flutter/material.dart';
+              import 'dart:math';
 
-              runApp(myApp());
-            }
+              void main(){
 
-  class  myApp  extends StatefulWidget {
-    @override
-    _State createState() => _State();
-  }
+                runApp(myApp());
+              }
 
-  class _State extends State<myApp> {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        theme: ThemeData(
-          primarySwatch: Colors.cyan,
-        ),
-        title: "TasDev",
-        home: MainPage(),
-        
-      );
+    class  myApp  extends StatefulWidget {
+      @override
+      _State createState() => _State();
     }
-  }
 
-
-  class MainPage extends StatefulWidget {
-    @override
-    _MainPageState createState() => _MainPageState();
-  }
-
-  class _MainPageState extends State<MainPage> {
-
-    int Value = 0;
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Main Page",
+    class _State extends State<myApp> {
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.cyan,
           ),
-
-        ),
-        
-
-        body: Center(
-
-            child: Column(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-
-
-                      children: <Widget>[
-                      
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          
-                          "$Value",
-                          
-                          style: TextStyle(
-                            fontSize: 20.5,
-                          
-                          ),
-                        
-                              
-                        ),
-                      ),
-                        
-
-                        RaisedButton(
-                        
-                          onPressed:  ChangeValue,
-                                                    color: Colors.cyan,
-                                                    child: Text(
-                          
-                                                                                  
-                                                      "Press Me ",
-                                                      style: TextStyle(
-                                                        color: Colors.black87,
-                                                        fontSize: 18.0,
-                                                      
-                                                      ),
-                                                    ),
-                                                  
-                          
-                          
-                                                  )
-                          
-                          
-                          
-                          
-                          
-                                                  ],
-                          
-                                      ),
-                          
-                          
-                                  ),
-                                );
-                              }
-                            
-                            void ChangeValue() {
-
-                              setState(() {
-                               Value += 1; 
-                              });
-  }
-}
-
-
+          title: "TasDev",
+          home: MainPage(),
           
+        );
+      }
+    }
+
+
+    class MainPage extends StatefulWidget {
+      @override
+      _MainPageState createState() => _MainPageState();
+    }
+
+    class _MainPageState extends State<MainPage> {
+  int index;
+  Color colorNow;
+      List<Color> value = [
+            Color(0xfff44336),
+              Color(0xeef44336),
+              Color(0xccf44336),
+                Color(0xddf44336),
+                Color(0xaaf44336),
+                  Color(0xaef44336),
+
+      ];
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+
+          backgroundColor: colorNow,
+          appBar: AppBar(
+            title: Text(
+              "Main Page",
+            ),
+
+          ),
+          
+
+          body: Center(
+            
+              
+              child: Column(
+
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                      
+                        children: <Widget>[
+                         
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            
+                            "We Will Change the Background Color",
+                            
+                            style: TextStyle(
+                              fontSize: 20.5,
+                            
+                            ),
+                          
+                                
+                          ),
+                        ),
+                          
+
+                          RaisedButton(
+                          
+                            onPressed:  ChangeColorValue,
+                                                      color: Colors.cyan,
+                                                      child: Text(
+                            
+                                                                                    
+                                                        "Press Me ",
+                                                        style: TextStyle(
+                                                          color: Colors.black87,
+                                                          fontSize: 18.0,
+                                                        
+                                                        ),
+                                                      ),
+                                                    )
+                                                    ],
+                            
+                                        ),
+                            
+                            
+                                    ),
+                                  );
+                                }
+                              
+                              void ChangeColorValue() {
+
+                                setState(() {
+                                
+                               index = Random().nextInt(value.length);
+                               colorNow = value[index];
+
+
+                                });
+                                }
+  }
+
+
+            
 
