794d2cc3a00712cc8bc4b3baa3f6803f0929b22c
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-10-16 12:38:51 +0600

    More Row & COllumn Works

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,252 @@
-import 'package:flutter/material.dart';
-void main(){
+          import 'package:flutter/material.dart';
+          void main(){
 
-  runApp(myApp());
-}
-
-
+            runApp(myApp());
+          }
 
 
- class myApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      
-      title: "Tasnuva App",
-      theme: ThemeData(
 
-        primarySwatch: Colors.red,
 
-      ),
+          class myApp extends StatelessWidget {
+            @override
+            Widget build(BuildContext context) {
+              return MaterialApp(
+                
+                title: "Tasnuva App",
+                theme: ThemeData(
+
+                  primarySwatch: Colors.red,
+
+                ),
+              
+                home: Scaffold(
+
+                  //our app bar which will be top of your app
+                appBar: AppBar(
+                title: Text(
+
+            "My Application",
+                ),
+                ),
+
+            body: Center(
+                child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+
+                          Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Text(
+                                "Hello this is full description of our message . This is mainly a paragrap for test purpose im showing this tasnuva oshin",
+                        style: TextStyle(
+                          fontSize: 20.5,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent,
+
+                        ),
+
+                            ),
+                          ),
+                        
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                        
+
+
+                            RaisedButton(
+                              onPressed: (){},
+                              color: Colors.green,
+                          child: Text(
+                            "Ok",
+                          style: TextStyle(
+
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+
+                        ),
+
+              RaisedButton(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
+
+                             fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+ 
+                        ),
+
+ RaisedButton(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
+
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+ 
+                        ),
+
+
+                        ],
+
+
+
+                        ),
+
+   Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                        
+
+
+                            RaisedButton(
+                              onPressed: (){},
+                              color: Colors.green,
+                          child: Text(
+                            "Ok",
+                          style: TextStyle(
+
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+
+                        ),
+
+              RaisedButton(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
+
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+ 
+                        ),
+
+ RaisedButton(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
+
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+ 
+                        ),
+
+
+                        ],
+
+
+
+                        ),
+
+
+                              Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Text(
+                                "Hello this is full description of our message . This is mainly a paragrap for test purpose im showing this tasnuva oshin",
+                        style: TextStyle(
+                         
+                          fontSize: 20.5,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.redAccent,
+
+                        ),
+
+                            ),
+                          ),
     
-      home: Scaffold(
+                     Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                        
 
-        //our app bar which will be top of your app
-      appBar: AppBar(
-      title: Text(
 
-  "My Application",
-      ),
-      ),
+                            RaisedButton(
+                              onPressed: (){},
+                              color: Colors.green,
+                          child: Text(
+                            "Ok",
+                          style: TextStyle(
 
-   body: Center(
-       child: Column(
-     
-     mainAxisAlignment: MainAxisAlignment.center,
-     crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
+                            fontSize: 20.0,
+                            
+                          ),
 
-                 Padding(
-                   padding: const EdgeInsets.all(30.0),
-                   child: Text(
-                      "Hello this is full description of our message . This is mainly a paragrap for test purpose im showing this tasnuva oshin",
-               style: TextStyle(
-                fontSize: 20.5,
-                fontWeight: FontWeight.bold,
-                color: Colors.redAccent,
+                          ),
 
-               ),
+                        ),
 
-                   ),
-                 ),
-               
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-               children: <Widget>[
-               
+                 RaisedButton(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
 
+                            fontSize: 20.0,
+                            
+                          ),
+
+                          ),
+ 
+                        ),
 
                   RaisedButton(
-                    onPressed: (){},
-                    color: Colors.green,
-                child: Text(
-                  "Ok",
-                style: TextStyle(
+                  onPressed: (){},
+                              color: Colors.red,
+                          child: Text(
+                            "Cancel",
+                          style: TextStyle(
 
-                  fontSize: 30.0,
-                  
-                ),
+                            fontSize: 20.0,
+                            
+                          ),
 
-                ),
+                          ),
+ 
+                        ),
 
-               ),
 
-     RaisedButton(
-         onPressed: (){},
-                    color: Colors.red,
-                child: Text(
-                  "Cancel",
-                style: TextStyle(
+                        ],
 
-                  fontSize: 30.0,
-                  
-                ),
+
+
+                        ),
+
+                    ],
 
                 ),
 
-               )
-
-
-
-               ],
-
-
-
-               )
-
-
-              
-
-
-
-
-
-
-          ],
-
-       ),
-
-
-   ),
-
-
-    ),
-    );
-  }
-}
+            ),
+              ),
+              );
+            }
+          }
 
