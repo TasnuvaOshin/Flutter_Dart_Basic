7a3e850b89af6450c38580d8e3eb03c0d04c135c
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-10-16 13:27:58 +0600

    Stateful Widget with button

--- lib/main.dart
+++ lib/main.dart
@@ -1,252 +1,113 @@
-          import 'package:flutter/material.dart';
-          void main(){
+            import 'package:flutter/material.dart';
+            void main(){
 
-            runApp(myApp());
-          }
-
-
-
-
-          class myApp extends StatelessWidget {
-            @override
-            Widget build(BuildContext context) {
-              return MaterialApp(
-                
-                title: "Tasnuva App",
-                theme: ThemeData(
-
-                  primarySwatch: Colors.red,
-
-                ),
-              
-                home: Scaffold(
-
-                  //our app bar which will be top of your app
-                appBar: AppBar(
-                title: Text(
-
-            "My Application",
-                ),
-                ),
-
-            body: Center(
-                child: Column(
-              
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-
-                          Padding(
-                            padding: const EdgeInsets.all(30.0),
-                            child: Text(
-                                "Hello this is full description of our message . This is mainly a paragrap for test purpose im showing this tasnuva oshin",
-                        style: TextStyle(
-                          fontSize: 20.5,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.redAccent,
-
-                        ),
-
-                            ),
-                          ),
-                        
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                        
-
-
-                            RaisedButton(
-                              onPressed: (){},
-                              color: Colors.green,
-                          child: Text(
-                            "Ok",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
-
-                        ),
-
-              RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                             fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
- RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
-
-                        ],
-
-
-
-                        ),
-
-   Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                        
-
-
-                            RaisedButton(
-                              onPressed: (){},
-                              color: Colors.green,
-                          child: Text(
-                            "Ok",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
-
-                        ),
-
-              RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
- RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
-
-                        ],
-
-
-
-                        ),
-
-
-                              Padding(
-                            padding: const EdgeInsets.all(30.0),
-                            child: Text(
-                                "Hello this is full description of our message . This is mainly a paragrap for test purpose im showing this tasnuva oshin",
-                        style: TextStyle(
-                         
-                          fontSize: 20.5,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.redAccent,
-
-                        ),
-
-                            ),
-                          ),
-    
-                     Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                        
-
-
-                            RaisedButton(
-                              onPressed: (){},
-                              color: Colors.green,
-                          child: Text(
-                            "Ok",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
-
-                        ),
-
-                 RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
-                  RaisedButton(
-                  onPressed: (){},
-                              color: Colors.red,
-                          child: Text(
-                            "Cancel",
-                          style: TextStyle(
-
-                            fontSize: 20.0,
-                            
-                          ),
-
-                          ),
- 
-                        ),
-
-
-                        ],
-
-
-
-                        ),
-
-                    ],
-
-                ),
-
-            ),
-              ),
-              );
+              runApp(myApp());
             }
-          }
+
+  class  myApp  extends StatefulWidget {
+    @override
+    _State createState() => _State();
+  }
+
+  class _State extends State<myApp> {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        title: "TasDev",
+        home: MainPage(),
+        
+      );
+    }
+  }
+
+
+  class MainPage extends StatefulWidget {
+    @override
+    _MainPageState createState() => _MainPageState();
+  }
+
+  class _MainPageState extends State<MainPage> {
+
+    int Value = 0;
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Main Page",
+          ),
+
+        ),
+        
+
+        body: Center(
+
+            child: Column(
+
+              mainAxisAlignment: MainAxisAlignment.center,
+
+
+                      children: <Widget>[
+                      
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          
+                          "$Value",
+                          
+                          style: TextStyle(
+                            fontSize: 20.5,
+                          
+                          ),
+                        
+                              
+                        ),
+                      ),
+                        
+
+                        RaisedButton(
+                        
+                          onPressed:  ChangeValue,
+                                                    color: Colors.cyan,
+                                                    child: Text(
+                          
+                                                                                  
+                                                      "Press Me ",
+                                                      style: TextStyle(
+                                                        color: Colors.black87,
+                                                        fontSize: 18.0,
+                                                      
+                                                      ),
+                                                    ),
+                                                  
+                          
+                          
+                                                  )
+                          
+                          
+                          
+                          
+                          
+                                                  ],
+                          
+                                      ),
+                          
+                          
+                                  ),
+                                );
+                              }
+                            
+                            void ChangeValue() {
+
+                              setState(() {
+                               Value += 1; 
+                              });
+  }
+}
+
+
+          
 
